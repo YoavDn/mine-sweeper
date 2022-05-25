3705497a25e3cb598be6730c9a382adc71697e94
yoav <yoavdan.m@gmail.com>
2022-05-25 17:33:03 +0300

    expent empty cells

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ const FLAG = '&ofcir;'
 var gBoard
 
 var gLevel = {
-  size: 4,
+  size: 12,
   mines: 2,
 }
 var gGame = {
@@ -69,6 +69,62 @@ function renderBoard(board) {
   boardContainer.innerHTML = strHtml
 }
 
+function cellClicked(elCell) {
+  var cellPos = getCelPos(elCell)
+
+  if (!gBoard[cellPos.i][cellPos.j].minesAround)
+    expandShown(gBoard, cellPos.i, cellPos.j)
+  if (gBoard[cellPos.i][cellPos.j].isShown === false) gGame.shownCount++
+
+  if (gBoard[cellPos.i][cellPos.j].isMine) gameOver()
+
+  gBoard[cellPos.i][cellPos.j].isShown = true
+
+  checkGameOver()
+  renderBoard(gBoard)
+}
+
+function getRendomMinePos(board = gLevel.size, mineCount = gLevel.mines) {
+  var minesPosArr = []
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board.length; j++) {
+      minesPosArr.push({ i, j })
+    }
+  }
+  shuffle(minesPosArr)
+  minesPosArr = minesPosArr.slice(-mineCount)
+
+  for (var i = 0; i < minesPosArr.length; i++) {
+    gBoard[minesPosArr[i].i][minesPosArr[i].j].isMine = true
+  }
+  console.log(minesPosArr)
+}
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5)
+}
+
+function markCell(elCell, e) {
+  e.preventDefault()
+  var cellPos = getCelPos(elCell)
+  var cell = gBoard[cellPos.i][cellPos.j]
+
+  if (cell.isMine) gGame.markedCount++
+
+  if (cell.isMarked) {
+    cell.isMarked = false
+  } else {
+    cell.isMarked = true
+  }
+
+  checkGameOver()
+  console.log(gGame.markedCount)
+
+  console.log(elCell)
+  console.log(gBoard[cellPos.i][cellPos.j])
+  renderBoard(gBoard)
+}
+
 function checkGameOver() {
   if (gGame.markedCount + gGame.shownCount === gLevel.size ** 2) {
     console.log('you won')
@@ -80,3 +136,43 @@ function gameOver() {
   console.log('you lost')
   return
 }
+
+function expandShown(board, posI, posJ) {
+  for (var i = posI - 1; i <= posI + 1; i++) {
+    if (i < 0 || i >= board.length) continue
+
+    for (var j = posJ - 1; j <= posJ + 1; j++) {
+      if (j < 0 || j >= board[i].length) continue
+
+      if (i === posI && j === posJ) continue
+
+      if (
+        !board[i][j].minesAround &&
+        !board[i][j].isShown &&
+        !board[i][j].isMine
+      ) {
+        board[i][j].isShown = true
+        gGame.isShown++
+        expandShown(board, i, j)
+      } else if (board[i][j].minesAround && !board[i][j].isShown) {
+        board[i][j].isShown = true
+
+        gGame.isShown++
+      } else continue
+    }
+  }
+}
+
+// function expandShown(board, posI, posJ) {
+//   for (var i = posI - 1; i <= posI + 1; i++) {
+//     if (i < 0 || i >= board.length) continue
+
+//     for (var j = posJ - 1; j <= posJ + 1; j++) {
+//       if (j < 0 || j >= board[i].length) continue
+
+//       if (i === posI && j === posJ) continue
+
+//      var cell = board[i][j]
+//     }
+//   }
+// }
--- js/utils.js
+++ js/utils.js
@@ -39,58 +39,8 @@ function getCelPos(cell) {
   }
 }
 
-function cellClicked(elCell) {
-  var cellPos = getCelPos(elCell)
-
-  if (gBoard[cellPos.i][cellPos.j].isShown === false) gGame.shownCount++
-
-  if (gBoard[cellPos.i][cellPos.j].isMine) gameOver()
-
-  gBoard[cellPos.i][cellPos.j].isShown = true
-
-  checkGameOver()
-  renderBoard(gBoard)
-}
-
-function getRendomMinePos(board = gLevel.size, mineCount = gLevel.mines) {
-  var minesPosArr = []
-  for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board.length; j++) {
-      minesPosArr.push({ i, j })
-    }
-  }
-  shuffle(minesPosArr)
-  minesPosArr = minesPosArr.slice(-mineCount)
-
-  for (var i = 0; i < minesPosArr.length; i++) {
-    gBoard[minesPosArr[i].i][minesPosArr[i].j].isMine = true
-  }
-  console.log(minesPosArr)
-}
-
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5)
-}
-
-function markCell(elCell, e) {
-  e.preventDefault()
-  var cellPos = getCelPos(elCell)
-  var cell = gBoard[cellPos.i][cellPos.j]
-
-  if (cell.isMine) gGame.markedCount++
-
-  if (cell.isMarked) {
-    cell.isMarked = false
-  } else {
-    cell.isMarked = true
-  }
-
-  checkGameOver()
-  console.log(gGame.markedCount)
-
-  console.log(elCell)
-  console.log(gBoard[cellPos.i][cellPos.j])
-  renderBoard(gBoard)
 }
 
 function addCellClass(cell, i, j) {
